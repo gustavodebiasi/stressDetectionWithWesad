a3a3cd6490f254b48b6b3dcf723119f1b353866b
Gustavo Debiasi <gustavo-debiasi@hotmail.com>
2019-08-24 22:49:40 -0300

    Development

---
+++ create_windows.py
@@ -0,0 +1,16 @@
+import os
+import pickle
+import numpy as np
+
+def execute(data_set_path):
+    print('executando')
+
+
+
+if __name__ == '__main__':
+    path = "/Volumes/My Passport/TCC/WESAD"
+    # path_input = input('Digite o caminho (padrão = "/Volumes/My Passport/TCC/WESAD"')
+    # if path_input:
+    #     path = path_input
+    # print("path = {}".format(path))
+    execute(path)
