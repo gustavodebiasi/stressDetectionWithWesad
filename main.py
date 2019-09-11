18d822da2215104a30d8b585dcfc4f118c8c88dd
Gustavo Debiasi <gustavo-debiasi@hotmail.com>
2019-09-10 23:41:43 -0300

    Development.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from reader import Reader
-from extractor import Extractor
-from selector import Selector
-from shooter import Shooter
+# from extractor import Extractor
+# from selector import Selector
+# from shooter import Shooter
 from Enums.Types import Types
 
 RUN_READER = True
@@ -9,38 +9,53 @@ RUN_EXTRACTOR = False
 RUN_SELECTOR = False
 RUN_SHOOTER = False
 
+BASE_PATH = '/Volumes/My Passport/TCC/WESAD/'
+BASE_SUBJECTS = [2]
+# SUBJECTS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17]
+
 # Reader Variables
-READER_PATH = '/Volumes/My Passport/TCC/WESAD/TESTE/'
+READER_PATH = BASE_PATH
 READER_TYPES = [
     Types.BASELINE.value,
     Types.STRESS.value,
     Types.AMUSEMENT.value,
 ]
-READER_SUBJECTS = [2]
+READER_SUBJECTS = BASE_SUBJECTS
+
 
 # Extractor Variables
-
+EXTRACTOR_PATH = BASE_PATH
+WINDOW_SIZE = 20
+WINDOW_OVERLAP = False
+EXTRACTOR_SUBJECTS = BASE_SUBJECTS
 
 # Selector Variables
+SELECTOR_PATH = BASE_PATH
 
+# Classifier Variables
+EXTRACTOR_PATH = BASE_PATH
+CLASSIFIER_SUBJECTS = BASE_SUBJECTS
 
 # Shooter Variables
-
+EXTRACTOR_PATH = BASE_PATH
+CLASSIFIER_SUBJECTS = BASE_SUBJECTS
 
 
 if __name__ == "__main__":
+    print("Inicia processo!")
+
     if (RUN_READER):
         read = Reader()
         read.execute(READER_PATH, READER_TYPES, READER_SUBJECTS)
 
-    if (RUN_EXTRACTOR):
-        extract = Extractor
-        extract.execute()
+    # if (RUN_EXTRACTOR):
+    #     extract = Extractor
+    #     extract.execute()
 
-    if (RUN_SELECTOR):
-        select = Selector
-        select.execute()
+    # if (RUN_SELECTOR):
+    #     select = Selector
+    #     select.execute()
 
-    if (RUN_SHOOTER):
-        shoot = Shooter
-        shoot.execute()
+    # if (RUN_SHOOTER):
+    #     shoot = Shooter
+    #     shoot.execute()
